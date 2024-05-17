12c93bca7dac21435831a7663e33d026553e50d2
Yan Brunshteyn <brunshte@gmail.com>
2024-05-17 10:18:25 -0700

    add upload progress view, gradient layer in background, timer to gradually phase out progress bar after test complete

--- SpeedTest/SpeedTest/ViewController.swift
+++ SpeedTest/SpeedTest/ViewController.swift
@@ -11,63 +11,92 @@ import SpeedcheckerReportSDK
 import CoreLocation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var runSpeedTestBtn: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
-    let circularProgressBar = CircularProgressView()
+    let downloadProgress = CircularProgressView(withType: "Download")
+    let uploadProgress = CircularProgressView(withType: "Upload")
     
     private var internetTest: InternetSpeedTest?
     private var locationManager = CLLocationManager()
     private var signalHelper: SCSignalHelper?
     
     let gradientLayer: CAGradientLayer = {
-            let layer = CAGradientLayer()
-            layer.colors = [
-                UIColor.red.cgColor,
-                UIColor.green.cgColor
-            ]
-            return layer
-        }()
-
+        let layer = CAGradientLayer()
+        layer.colors = [
+            UIColor.red.cgColor,
+            UIColor.green.cgColor
+        ]
+        return layer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.layer.addSublayer(gradientLayer)
-//        gradientLayer.frame = view.bounds
+        view.layer.addSublayer(gradientLayer)   // First: added as sublayer
+        gradientLayer.frame = view.bounds       // Second: frame set frame is pos & size relative to superlayer's coord space, & bounds are rel to it's own coord space
         runSpeedTestBtn.translatesAutoresizingMaskIntoConstraints = false
         runSpeedTestBtn.layer.zPosition = 1
         
         progressBar.isHidden = true
         
-//        circularProgressBar.translatesAutoresizingMaskIntoConstraints = false
-        circularProgressBar.progressColor = UIColor.blue
-        circularProgressBar.trackColor = .systemMint
-        view.addSubview(circularProgressBar)
-//        NSLayoutConstraint.activate([
-//            circularProgressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            circularProgressBar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            circularProgressBar.widthAnchor.constraint(equalToConstant: 125),
-//            circularProgressBar.heightAnchor.constraint(equalToConstant: 125)
-//        ])
-        circularProgressBar.frame.size.width = 125
-        circularProgressBar.frame.size.height = 125
-        circularProgressBar.center = self.view.center
+        configureDownloadProgressBar()
+        configureUploadProgressBar()
         
-
         requestLocationAuthorization()
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        
-//        gradientLayer.frame = view.bounds
-//    }
+    //    override func viewDidLayoutSubviews() {
+    //        super.viewDidLayoutSubviews()
+    //
+    //        gradientLayer.frame = view.bounds
+    //    }
+    
+    func configureDownloadProgressBar() {
+        downloadProgress.translatesAutoresizingMaskIntoConstraints = false
+        downloadProgress.progressColor = UIColor.blue
+        downloadProgress.trackColor = .systemMint
+        view.addSubview(downloadProgress)
+        
+        // Uses Auto Layout to setup size and location
+        NSLayoutConstraint.activate([
+            downloadProgress.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -(view.frame.size.width/4)),
+            downloadProgress.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            downloadProgress.widthAnchor.constraint(equalToConstant: 125),
+            downloadProgress.heightAnchor.constraint(equalToConstant: 125)
+        ])
+        
+        // Uses Frame to setup size and location
+        //        downloadProgress.frame.size.width = 125
+        //        downloadProgress.frame.size.height = 125
+        //        downloadProgress.center = self.view.center
+    }
+    
+    func configureUploadProgressBar() {
+        uploadProgress.translatesAutoresizingMaskIntoConstraints = false
+        uploadProgress.progressColor = UIColor.blue
+        uploadProgress.trackColor = .systemMint
+        view.addSubview(uploadProgress)
+        
+        // Uses Auto Layout to setup size and location
+        NSLayoutConstraint.activate([
+            uploadProgress.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: (view.frame.size.width/4)),
+            uploadProgress.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            uploadProgress.widthAnchor.constraint(equalToConstant: 125),
+            uploadProgress.heightAnchor.constraint(equalToConstant: 125)
+        ])
+        
+        // Uses Frame to setup size and location
+        //        uploadProgress.frame.size.width = 125
+        //        uploadProgress.frame.size.height = 125
+        //        uploadProgress.center = self.view.center
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        circularProgressBar.speedLabel.isHidden = true
+        //        downloadProgress.speedLabel.isHidden = true
     }
-
+    
     @IBAction func runSpeedTestTouched(_ sender: UIButton) {
         // to use free version, your app should have location access
         internetTest = InternetSpeedTest(delegate: self)
@@ -78,15 +107,33 @@ class ViewController: UIViewController {
         }
         
         // to use paid version, your app does not need location access
-//        internetTest = InternetSpeedTest(licenseKey: "Your license key", delegate: self)
-//        internetTest?.start() { (error) in
-//          if error != .ok {
-//              print("Error: \(error.rawValue)")
-//          }
-//        }
+        //        internetTest = InternetSpeedTest(licenseKey: "Your license key", delegate: self)
+        //        internetTest?.start() { (error) in
+        //          if error != .ok {
+        //              print("Error: \(error.rawValue)")
+        //          }
+        //        }
         signalHelper = SCSignalHelper()
         let signalStrength = signalHelper?.getSignalStrengh()
         print("signalStrength: \(String(describing: signalStrength?.wiFiStrength?.convertToDB() ?? 0))")
+    }
+    
+    func phaseOutProgressBars() {
+        var timeLeft = CGFloat(0)
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] timer in
+            DispatchQueue.main.async {
+                self?.downloadProgress.progressLayer.strokeEnd = CGFloat(1/timeLeft)
+                self?.uploadProgress.progressLayer.strokeEnd = CGFloat(1/timeLeft)
+            }
+            timeLeft += 1
+            if(timeLeft==100){
+                DispatchQueue.main.async {
+                    self?.downloadProgress.progressLayer.strokeEnd = CGFloat(0)
+                    self?.uploadProgress.progressLayer.strokeEnd = CGFloat(0)
+                }
+                timer.invalidate()
+            }
+        }
     }
     
     func requestLocationAuthorization() {
@@ -104,6 +151,8 @@ class ViewController: UIViewController {
     
 }
 
+
+// MARK: - InternetSpeedTestDelegate
 extension ViewController: InternetSpeedTestDelegate {
     func internetTestError(error: SpeedTestError) {
         print("Error: \(error.rawValue)")
@@ -134,11 +183,10 @@ extension ViewController: InternetSpeedTestDelegate {
     }
     
     func internetTestDownload(progress: Double, speed: SpeedTestSpeed) {
-//        progressBar.progress = Float(progress)
-        circularProgressBar.progressLayer.strokeEnd = progress
-        print("circularProgressBar.progressLayer.strokeEnd = \(progress)")
-//        circularProgressBar.speedLabel.text = "\(Int(speed.mbps)) Mbps"
-        circularProgressBar.speed = Int(speed.mbps)
+        downloadProgress.progressLayer.strokeEnd = progress
+        print("downloadProgress.progressLayer.strokeEnd = \(progress)")
+        //        downloadProgress.speedLabel.text = "\(Int(speed.mbps)) Mbps"
+        downloadProgress.speed = Int(speed.mbps)
         print("Download: \(speed.descriptionInMbps)")
     }
     
@@ -147,13 +195,17 @@ extension ViewController: InternetSpeedTestDelegate {
     }
     
     func internetTestUploadFinish() {
-        //
+        phaseOutProgressBars()
     }
     
     func internetTestUpload(progress: Double, speed: SpeedTestSpeed) {
+        uploadProgress.progressLayer.strokeEnd = progress
+        print("uploadProgress.progressLayer.strokeEnd = \(progress)")
+        //        uploadProgress.speedLabel.text = "\(Int(speed.mbps)) Mbps"
+        uploadProgress.speed = Int(speed.mbps)
         print("Upload: \(speed.descriptionInMbps)")
     }
 }
-
+// MARK: - CLLocationManagerDelegate
 extension ViewController: CLLocationManagerDelegate {
 }
